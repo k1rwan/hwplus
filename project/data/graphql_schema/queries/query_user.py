9fbfdd1f7f636161ea5cc0c5d854c2567a7983eb
liadrinz <435438602@qq.com>
2018-10-27 15:38:25 +0800

    加接口getUsersByCourseId

--- project/data/graphql_schema/queries/query_user.py
+++ project/data/graphql_schema/queries/query_user.py
@@ -1,5 +1,6 @@
 import graphene
 from data.graphql_schema.types import *
+from data import models
 
 class QueryUser(object):
 
@@ -7,6 +8,7 @@ class QueryUser(object):
     get_users_by_ids = graphene.List(UserType, ids=graphene.List(of_type=graphene.Int))
     get_users_by_usertype = graphene.List(UserType, usertype=graphene.String())
     get_users_by_usernames = graphene.List(UserType, usernames=graphene.List(of_type=graphene.String))
+    get_users_by_course_id = graphene.List(UserType, course_id=graphene.Int())
 
     # query for users
     def resolve_all_users(self, info, **kwargs):
@@ -26,3 +28,8 @@ class QueryUser(object):
         for item in kwargs['usernames']:
             result = result | models.models.Q(username=item)
         return models.User.objects.filter(result)
+
+    def resolve_get_users_by_course_id(self, info, **kwargs):
+        target_course = models.HWFCourseClass.objects.get(pk=kwargs['course_id'])
+        return target_course.students.all()
+
